dd26f5469b5b217d66a1061114ea72b9aa10bb5b
neotruong <neo.truong@uberfusion.com>
2020-12-15 09:36:51 +0700

    [] first init

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+ 
+var app = express();
+ 
+app.use(express.static("public"));
+ 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+ 
+app.listen(3000);
+ 
+app.get("/", function(request, response)  {
+    
+    response.render("homePage");
+});
+ 
